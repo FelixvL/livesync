8752dc311d9a600f579570d80847fcf53673d942
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-11-05 22:18:40 +0100

    next

--- test.py
+++ test.py
@@ -1,20 +1,2 @@
 import geheim
-import os
-import openai
-
-
-from openai import OpenAI
-client = OpenAI(api_key=geheim.key())
-invoer = input("stel een vraag? ")
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[{
-      "role": "system",
-      "content": '''vertel in spaans over: '''+invoer+''''''
-    }],
-    temperature=0.8,
-    max_completion_tokens=1024
-)
-
-print(completion)
-# print(completion.choices[0].message.content)
+geheim.key()
