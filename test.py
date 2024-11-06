e32ed0cfe7a36d02bb825d85aa869d1cffbc7bd0
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-11-06 16:12:57 +0100

    next

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-import geheim
-print(geheim.key())
+import requests
+
+url = "https://catfact.ninja/fact"
+    
+# Doe de API-aanroep
+response = requests.get(url)
+
+data = response.json()
+cat_fact = data.get("fact")
+
+print("Kattenfeitje: ", cat_fact)
