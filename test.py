0d934c13b525aae42420012b8911f3fd65c70a1b
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-11-06 17:01:26 +0100

    next

--- test.py
+++ test.py
@@ -5,17 +5,24 @@ import geheim
 
 from openai import OpenAI
 client = OpenAI(api_key=geheim.key())
-invoer = input("Stel hier uw vraag? ")
+
 response = client.chat.completions.create(
-  model="gpt-4",
-  messages=[{
-      "role": "system",
-      "content": ""+invoer
-    }],
-    temperature=0.5,
-    max_tokens=2024,
-    stream=True
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What's in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                    }
+                },
+            ],
+        }
+    ],
+    max_tokens=300,
 )
 
-for chunk in response:
-    print(chunk.choices[0].delta.content, end="", flush=True)
+print(response.choices[0])
