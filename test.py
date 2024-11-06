24b0ea6f26e48a287d0e3cac55ac37f869c0e99c
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-11-06 16:14:33 +0100

    next

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-import requests
+import os
+import openai
+import geheim
 
-url = "https://catfact.ninja/fact"
-    
-# Doe de API-aanroep
-response = requests.get(url)
 
-data = response.json()
-cat_fact = data.get("fact")
+from openai import OpenAI
+client = OpenAI(api_key=geheim.key())
+invoer = input("stel een vraag? ")
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[{
+      "role": "system",
+      "content": ''''''+invoer+''''''
+    }],
+    temperature=0.8,
+    max_completion_tokens=1024
+)
 
-print("Kattenfeitje: ", cat_fact)
+print(completion)
+# print(completion.choices[0].message.content)
