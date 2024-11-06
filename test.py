f0c8b7e925c4b6f3546311072281b39e29ebbe25
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-11-06 16:36:32 +0100

    next

--- test.py
+++ test.py
@@ -5,16 +5,23 @@ import geheim
 
 from openai import OpenAI
 client = OpenAI(api_key=geheim.key())
-invoer = input("noem een stad? ")
-completion = client.chat.completions.create(
-  model="gpt-4",
-  messages=[{
-      "role": "system",
-      "content": '''Ik heet Felix en spreek mij formeel aan. Noem de 5 beroemdste inwoners van : '''+invoer+''', en vertel er in het kort iets over.Ik wil de tekst in het Italiaans'''
-    }],
-    temperature=0.8,
-    max_completion_tokens=600
+
+# Laad het bestand in als tekst (bijvoorbeeld JSON of tekst)
+with open("films.csv", "r") as file:
+    file_content = file.read()
+
+# Stel de vraag met de inhoud van het bestand als context
+vraag = input("stel een vraag over het bestand: ")
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "system", "content": "Dit is de inhoud van het bestand: " + file_content},
+        {"role": "user", "content": vraag}
+    ],
+    temperature=0.7,
+    max_tokens=3200
 )
 
-# print(completion)
-print(completion.choices[0].message.content)
+# Print het antwoord van de API
+print(response)
+print(response.choices[0].message.content)
