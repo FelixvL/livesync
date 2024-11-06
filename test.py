576f9209edd32edb005538494b45b896783b1b65
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-11-06 16:46:35 +0100

    next

--- test.py
+++ test.py
@@ -12,9 +12,9 @@ completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[{
       "role": "system",
-      "content": '''JE BENT ONDERDEEL VAN EEN SYSTEEM: maak een website met informatie over: 
+      "content": '''JE BENT ONDERDEEL VAN EEN SYSTEEM: maak een object met verschillende properties over: 
         '''+invoer+''' 
-        IK WIL UITSLUITEND HTML. GEEN INTRO. GEEN UITRO. maak het mooi met css in vrolijke kleuren.'''
+        IK WIL UITSLUITEND JSON. GEEN INTRO. GEEN UITRO.'''
     #   "content": ''''''+invoer+''''''
     }],
     temperature=0.8,
