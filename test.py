5b0149d75d750b89847a954364e115290adb7d94
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-11-06 16:23:01 +0100

    next

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[{
       "role": "system",
-      "content": '''Noem de 5 beroemdste inwoners van : '''+invoer+''''''
+      "content": '''Noem de 5 beroemdste inwoners van : '''+invoer+''', en vertel er in het kort iets over.'''
     }],
     temperature=0.8,
     max_completion_tokens=1024
