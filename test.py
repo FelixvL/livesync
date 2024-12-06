97e07969d6c27331c2e5eb30f100b50e4ad5bc32
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-12-06 23:15:19 +0100

    next

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ response = client.chat.completions.create(
         {
             "role": "user",
             "content": [
-                {"type": "text", "text": "What's in this image?"},
+                {"type": "text", "text": "What''s in this image?"},
                 {
                     "type": "image_url",
                     "image_url": {
