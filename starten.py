246dd7674b99efdb0a8035c07ecf42f13b8a6e9d
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2025-01-22 13:40:03 +0100

    next

--- starten.py
+++ starten.py
@@ -1,1 +1,27 @@
 print("hij doet het")
+# Procedureel Programmeren
+# jaren 90    Java heel radicaal
+# Object Oriented Programming
+
+# het is succesvolle manier 
+# van programeurs om de wereld
+# zoals wij die waarnemen 
+# om te zetten in broncode
+
+# class    cakevorm  ovenschaal bouwtekening mal    1
+# object   cakeje    lasagna     huis        uitgietsel  oneindig
+
+# kenmerk
+# lichtje lampje      synoniem
+# bank                homoniem
+
+# een mens heeft het (kenmerk) dat hij een voornaam - elk lid
+# een (waarde van een kenmerk) van hem is dat hij felix heet - een lid
+
+# class kenmerk
+# object waarden van die kenmerken vast
+
+class Auto:
+	kleur = "ntb"
+
+
