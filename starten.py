88a363c3702a6a230230059e4a8caf4cbc9f1f46
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2025-01-22 10:19:58 +0100

    next

--- starten.py
+++ starten.py
@@ -1,1 +1,14 @@
+
+	       # parameter
+def uitvoeren(getal, tweede):
+	print("letsgo", getal)
+	print(getal + tweede)
+	return "maandag"
+	# returntype
 print("doen")
+uitvoeren(5, 35) # argument
+uitvoeren(17, 66)
+uitkomst = uitvoeren(12, 55)
+print(uitkomst)
+print( uitvoeren(4,6) )
+print("go")
