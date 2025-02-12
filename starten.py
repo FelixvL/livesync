0a4a8e39b07b77c9b9a0c4741b691a14f7c6ec74
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2025-02-12 19:25:38 +0100

    next

--- starten.py
+++ starten.py
@@ -1,2 +1,11 @@
-print("hoi!??")
+import requests
 
+url = "https://catfact.ninja/fact"
+
+# Doe de API-aanroep
+response = requests.get(url)
+
+data = response.json()
+cat_fact = data.get("fact")
+
+print("Kattenfeitje: ", cat_fact)
